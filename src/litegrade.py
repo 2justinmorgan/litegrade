eab0c9a63990cfdceb4f844c5548cd80d48201e5
Justin_Morgan <2justinmorgan@gmail.com>
2020-08-27 04:15:24 -0700

    Decomposed litegrade.py by implementing a commondriver.py BRANCH: feature/Hello_World_Interdependent_Module_Scripts

--- src/litegrade.py
+++ src/litegrade.py
@@ -1,36 +1,15 @@
 #!/home/jmorga27/Cal_Poly/Kurfess/litegrade/bin/python3
 
 # litegrade.py 
-#   This script contains student-interfacing functions that are seen at the 
+#   This script contains student-interfacing functions that can be seen at the 
 #   notebook level
 
-import os
-import sys
-import inspect
 import json
 
-msg_prefix = "litegrade"
-
-def print_err(msg, **kwargs):
-	frame = inspect.stack()[1]
-	caller_line_num = inspect.getframeinfo(inspect.stack()[1][0]).lineno
-	sys.stderr.write(f"{msg_prefix}: (line {caller_line_num}) {msg}\n")
-
-	callback = kwargs.get("callback", None)
-	if callback != None:
-		callback()
-
-def safe_fopen(fname, mode):
-	if not os.path.isfile(fname):
-		print_err(f"'{fname}' not found", callback=lambda: exit(1))
-
-	try:
-		fobj = open(fname, mode=mode)
-	except OSError as err:
-		print_err(f"'{fname}' open error")
-		print_err(f"(OS error below)\n{err}", callback=lambda: exit(1))
-
-	return fobj
+from importlib import reload
+import commondriver
+commondriver = reload(commondriver)
+from commondriver import print_err, safe_fopen
 
 def load_questions(json_fname):
 

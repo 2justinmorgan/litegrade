d10dbc86cdb16181f8ba36b4ec45d5c7152b5f05
Justin_Morgan <2justinmorgan@gmail.com>
2020-10-30 00:19:23 -0700

    Implemented get_html func in htmldriver BRANCH: feature/Ask_and_Check_User_Facing_Functions

--- src/htmldriver.py
+++ src/htmldriver.py
@@ -3,6 +3,10 @@
 #   This script creates and edits string representations of html elements
 #   that will later be rendered and displayed via IPython
 
+from modulemanager import \
+	load_questions, \
+	get_nested_value
+
 def create_node(tag_str, attributes_obj, inner_text_str):
 	node = f"<{tag_str}"
 	for attribute_name in attributes_obj:
@@ -57,5 +61,17 @@ def get_javascript(question_name):
 	</script>
 	""" % (question_name)
 	return javascript
+
+def get_html(question_name):
+	questions_obj = load_questions("questions.json")
+	question_obj = get_nested_value(["questions",question_name],questions_obj)
+
+	prompt_str = get_nested_value(["prompt"], question_obj)
+	label_type_str = get_nested_value(["choices_label_type"], question_obj)
+	choices_obj_lst = get_nested_value(["choices"], question_obj)
+	choices_lst = [o.get("description","") for o in choices_obj_lst]
+
+	return create_question_node( \
+		question_name, prompt_str, label_type_str, choices_lst)
 
 
